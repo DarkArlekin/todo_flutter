8a0c39da55ffd596e8380204506723f19e431fcf
DarkFoxIt <nikolaygaginua@gmail.com>
2022-04-13 01:58:24 +0300

    appbar fix

--- lib/todo_overview/widgets/app_bar.dart
+++ lib/todo_overview/widgets/app_bar.dart
@@ -2,5 +2,14 @@ import 'package:flutter/material.dart';
 
 class TodoAppBar extends AppBar {
   TodoAppBar({Key? key})
-      : super(key: key, title: const Center(child: Text("My Todo")), toolbarHeight: 48);
+      : super(
+            key: key,
+            title: const Text("My Todo"),
+            toolbarHeight: 48,
+            actions: [
+              IconButton(
+                onPressed: () => {},
+                icon: const Icon(Icons.add),
+              )
+            ]);
 }
